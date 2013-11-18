9cbc5f7fb8b3be25e7f986b0d7a6495b5ef7a7b6
ryanm <ryanm@zoovy.com>
2013-11-18 15:53:30 -0800

    Matching price Work

--- extensions/store_bmo.js
+++ extensions/store_bmo.js
@@ -98,6 +98,23 @@ var store_bmo = function() {
 				}
 			},
 			
+			rendermatchingBasePrice : {
+				onSuccess : function(rd) {
+			app.u.dump('--> response data:'); app.u.dump(rd);
+					var dataObj = {};
+					var matchPrice = parseFloat(app.data[rd.datapointer]['%attribs']['zoovy:base_price'],10).toFixed(2);
+			app.u.dump('--> the matching price:'); app.u.dump(matchPrice); 
+					var origPrice = parseFloat(rd.price,10).toFixed(2);
+			app.u.dump('--> the original price:'); app.u.dump(origPrice); 
+					dataObj.combindedTotal = origPrice + matchPrice;
+			app.u.dump('--> the final price:'); app.u.dump(dataObj.combindedTotal); 
+					rd.$container.anycontent({"templateID":rd.loadsTemplate,"data":dataObj});
+				},
+				onError : function(rd) {
+					app.u.dump('Error in extenstion: store_bmo rendermatchingBasePrice');
+				}
+			},
+			
 			renderProductsAsList : {
 				onSuccess : function(responseData) {
 			//		app.u.dump(app.data[responseData.datapointer]);
@@ -503,9 +520,30 @@ var store_bmo = function() {
 		
 			matchingBasePrice : function($tag, data) {
 				app.u.dump("bmoMoney--------------------->"); app.u.dump(data.value);
+				
 				var basePrice = (data.bindData.isElastic) ? data.value.base_price : data.value['%attribs']['zoovy:base_price'];
 				var match = app.u.makeSafeHTMLId((data.bindData.isElastic) ? data.value.matching_piece : data.value['%attribs']['user:matching_piece']);
+				if(match) {
+					var obj = {
+						pid	: match
+					};
 				
+					var _tag = {
+						"callback"		:"rendermatchingBasePrice",		
+						"extension"		:"store_bmo",			
+						"$container" 	: $tag,
+						"loadsTemplate"	: "matchingPriceTemplate",
+						"price"			: basePrice
+					};
+					app.calls.appProductGet.init(obj, _tag, 'immutable');
+					app.model.dispatchThis('immutable');
+				}
+				
+		/*		var dataObj = {};
+				dataObj.combinedTotal : basePrice + match.base_price;
+				
+				$tag.anycontent({'templateID':'matchingPriceTemplate','data':'dataObj'});
+		*/		
 				//$tag.append(app.u.formatMoney(matching_piece));
 			},
 		
