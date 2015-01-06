642b9842ad741c6f9dc63b8fdefab89b9b9d3935
rmurray <ryanm@zoovy.com>
2015-01-06 11:50:47 -0800

    201411 store_routing: removed custom code

--- extensions/store_routing.js
+++ extensions/store_routing.js
@@ -33,24 +33,6 @@ var store_routing = function(_app) {
 			onSuccess : function()	{
 				var r = false; 
 
-				//shows search result for any product with the tag passed and the is_app attribute
-				_app.router.addAlias('apptagsearch', 		function(routeObj){
-					var tag = routeObj.params.tag.toUpperCase(); 
-					showContent('search',{'elasticsearch':{'filter':{'and':{'filters':[{'term':{'tags':tag}},{'term':{'is_app':'1'}}]}}}})
-				});
-				//shows search results for any product with the attribute that is passed and set to "1", and the is_app attribute
-				_app.router.addAlias('appattribsearch',		function(routeObj){
-					var elasticsearch = {'filter':{'and':[{'term':{'is_app':'1'}}]}};
-					var fObj = {'term':{}};
-					fObj.term[routeObj.params.attrib] = '1';
-					elasticsearch.filter.and.push(fObj);
-					showContent('search',{'elasticsearch':elasticsearch});
-				});
-
-
-				_app.router.appendHash({'type':'exact','route':'/','callback':'homepage'});
-				_app.router.appendHash({'type':'match','route':'apptag/{{tag}}*','callback':'apptagsearch'}); //elastic search for tag plus is_app
-				_app.router.appendHash({'type':'match','route':'appattrib/{{attrib}}*','callback':'appattribsearch'}); //elastic search for attrib plus is_app
 				r = true;
 
 				return r;
@@ -58,81 +40,8 @@ var store_routing = function(_app) {
 			onError : function()	{
 				_app.u.dump('BEGIN store_routing.callbacks.init.onError');
 				}
-				dump("START store_routing.callbacks.attachEventHandlers.onSuccess");
-				$.merge(_app.ext.seo_robots.vars.pages,[
-	//TODO: Check if there are any more cusome pages that need to be added to robots_seo
-					"#!company/about/",
-					"#!company/contact/"
-				]);
-				var callback = function(event, $context, infoObj) {
-					dump("--> store_routing complete event");
-					event.stopPropagation();
-					if(infoObj) {
-						var hash = "";
-						var $routeEle = $('[data-routing-hash]',$context);
-						if($routeEle.length) {
-							dump('Whadda you know... there was a routing element');
-							hash = $routeEle.attr('data-routing-hash');
-						else {
-							switch(infoObj.pageType) { //for bmo: make all url's lower case.
-								case 'homepage':
-									hash = "#!/";
-									break;
-								case 'product':
-									//for bmo: append prod_name.html to end of hash
-									hash = "#!/product/"+infoObj.pid.toLowerCase()+"/"+infoObj.name;
-									break;
-								case 'category':
-									dump('There was no routing element');
-									hash = "#!/category/"+infoObj.navcat.toLowerCase()+"/";
-									break;
-								case 'static':
-									hash = window.location.hash;
-									break;
-								case 'search':
-									hash = window.location.hash.toLowerCase(); //"#!/search/"+encodeURIComponent(infoObj.KEYWORDS)+"/"
-									break;
-								case 'company':
-									hash = "#!company/"+infoObj.show.toLowerCase()+"/"
-									break;
-								case 'customer':
-									hash = "#!customer/"+infoObj.show.toLowerCase()+"/";
-									break;
-								case 'cart':
-									hash = "#!cart/";
-									break;
-								case 'checkout':
-									hash = "#!checkout/";
-									break;
-/*	TODO: FIND OUT IF SPECIAL PAGE TYPE FOR FILTER SEARCH CATEGORY IS AVAILABLE									
-											_app.templates.categoryTemplateFilteredSearch.on('complete.routing', function(event, $context, infoObj){
-												var hash = "";
-												var $routeEle = $('[data-routing-hash]',$context)
-												if($routeEle.length){
-													hash = $routeEle.attr('data-routing-hash');
-													}
-												else {
-													hash = "#!/category/"+infoObj.navcat+"/";
-												}
-												_app.ext.store_routing.u.setHash(hash);
-												});
-*/
-							}
-						}
-						_app.ext.store_routing.u.setHash(hash);
-					}
-				}
-				for(var i in _app.templates) {
-					_app.templates[i].on('complete.routing',callback);
-				}
-				$('#appTemplates').children().each(function(){
-					$(this).on('complete.routing',callback);
-				});
-		
-				
-			},
-		}	
-	}, //callbacks
+			}
+		}, //callbacks
 
 
 
@@ -209,28 +118,20 @@ optional params:
 		u : {
 			setHash : function(hash){
 				dump('setting hash to: '+hash);
-				var $canonical = $('link[rel=canonical]');
-				if(!$canonical.length){
-					dump('NO CANONICAL IN THE DOCUMENT'); dump(hash);
-					$canonical = $('<link rel="canonical" href="" />');
-					$('head').append($canonical);
-				}
-				$canonical.attr('href',hash);
-				if(_app.vars.showContentHashChange) {
+				
 				},
 			cleanURIComponent : function(str){
+				//trims whitespace
 				var component = str.replace(/^\s+|\s+$/g, '');
-				//component = component.replace(' ', '-');
+				//replaces all non alphanumerics with dashes
 				component = component.replace(/[^a-zA-Z0-9]+/g, '-');
 				return component;
 				},
-	//for bmo: make hash lower case.
 			productAnchor : function(pid, seo){
-				//for bmo: append .html to end of seo. 
-				return "#!product/"+pid.toLowerCase()+"/"+(seo ? _app.ext.store_routing.u.cleanURIComponent(seo).toLowerCase() : '')+".html";
+				return "/product/"+pid+"/"+(seo ? _app.ext.store_routing.u.cleanURIComponent(seo) : '');
 				},
 			categoryAnchor : function(path,seo)	{
-				return "#!category/"+path.toLowerCase()+"/"+((seo) ? _app.ext.store_routing.u.cleanURIComponent(seo).toLowerCase() : '');
+				if(path.charAt(0) == '.'){
 					path = path.substr(1);
 					}
 				return "/category/"+path+"/"+((seo) ? _app.ext.store_routing.u.cleanURIComponent(seo) : '');
@@ -238,10 +139,10 @@ optional params:
 			searchAnchor : function(type,value)	{
 				var r;
 				if(type == 'tag')	{
-					r = '#!search/tag/'+value.toLowerCase();
+					r = '/search/tag/'+value;
 					}
 				else if(type == 'keywords')	{
-					r = '#!search/keywords/'+value.toLowerCase();
+					r = '/search/keywords/'+value;
 					}
 // ### FUTURE -> support ability to search for a match on a specific attribute.
 //				else if(type == 'attrib')	{
@@ -250,7 +151,7 @@ optional params:
 				else	{
 					//unrecognized type
 					}
-				return "#!category/"+path.toLowerCase()+"/"+((seo) ? _app.ext.store_routing.u.cleanURIComponent(seo).toLowerCase() : '');
+				return "/category/"+path+((seo) ? "/"+encodeURIComponent(seo) : '');
 				}
 			}, //u [utilities]
 
