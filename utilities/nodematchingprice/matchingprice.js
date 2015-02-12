2b2378042b17d6537b11da4dfc88dbab5bcb1ce5
rmurray <ryanm@zoovy.com>
2015-02-11 21:30:05 -0800

    node matching price generator loop fix
    Made loop work, as well as more efficient.

--- utilities/nodematchingprice/matchingprice.js
+++ utilities/nodematchingprice/matchingprice.js
@@ -39,11 +39,12 @@ var initArray = [];				//used to hold appSEOFetch and appProductGet data when co
 var seoURLcount = 0;		//holds a count of how many urls were made w/ custom seo attrib
 var nameURLcount = 0;	//holds a count of how many urls were made w/ prod_name
 
+if(prods) {console.log('IT has begun. You will see a done message when IT has ended');}
 //itterate through the products in the response from appSEOFetch and send for appProductGet if indexable
 for(var j in prods['@OBJECTS']) {
-	var thisURLs = prods['@OBJECTS'][j];
-	if(!thisURLs['noindex'] && thisURLs.type === "pid") {
-		getSets(thisURLs);
+	var thisPROD = prods['@OBJECTS'][j];
+	if(!thisPROD['noindex'] && thisPROD.type === "pid" && (thisPROD.id.slice(-2) === "ST" || thisPROD.id.slice(-2) === "SB")) {
+		getSets(thisPROD);
 	}
 }
 
@@ -51,52 +52,46 @@ for(var j in prods['@OBJECTS']) {
 ce.dispatch(function(data) { processinitArray(initArray); });
 
 
-function getSets(thisObj) {
+function getSets(thisObj) { 
 ce.enqueue({
 		"pid": thisObj.id,
 		"_cmd": "appProductGet"
 	}, 
 	function(response){
-		if(!response.noindex && response.pid != undefined && (response.pid.slice(-2) === "ST" || response.pid.slice(-2) === "SB")) { // TODO : change this 4 to a 2, and change ..SR to ST and SB for bmo
-//			console.log(response.pid)
-			var L = response.pid.length;
-			thisObj.base = response.pid.slice(0,L - 2); // TODO : change this 4 to a 2 for bmo
-			thisObj.suffix = response.pid.slice(-2); // TODO : change this 4 to a 2 for bmo
-			thisObj.basePrice = response['%attribs']['zoovy:base_price'];
-			initArray.push(thisObj);
-		}
-	});	
+//		console.log(response.pid)
+		var L = response.pid.length;
+		thisObj.base = response.pid.slice(0,L - 2);
+		thisObj.suffix = response.pid.slice(-2);
+		thisObj.basePrice = response['%attribs']['zoovy:base_price'];
+		initArray.push(thisObj);		
+	});
 }
 
-
+//pulls last item in the array passed and compares it's base value to all other items.
+//if a match is found, basePrices for each item are summed and stored in each item.
+//each item is stored in an array that is then processed into a csv for product power tool.
+//if a match is not found, the item was removed from the array anyway and the next item is processed.
 function processinitArray(prods) {
 //	console.log(prods);
-	var today = new Date(); 
-	var datestr = dateFormat(now,"yyyy-mm-dd");
-	//for(var i in prods) {
+	var hunter, prey, matchPrice;
 	while(prods.length) {
-		var res = prods[0];
-		switch(res.type) {
-			case "pid" :
-				var prey = res.suffix === "ST" ? res.base+"SB" : res.base+"ST"; // TODO : set these to "ST" and "SB" for bmo
-				for(var k in prods) {
-					if(prods[k].id === prey) {
-						var matchPrice = Number(prods[k].basePrice) + Number(res.basePrice);
-						res.matchPrice = matchPrice+'';
-						prods[k].matchPrice = matchPrice+'';
-						MATCHES.push(prods[k]);
-						MATCHES.push(res);
-						prods.splice(0,1); //remove this product
-						prods.splice(k-1,1); //remove the matching piece for this product (index is 1 shorter because first .splice() shortened array)
-					}
-				}
-				break;
-			default : 
-				console.log("How did we get into default?"); console.log(res);
-				break;
+		hunter = prods.pop(); 
+		for(var k in prods) {
+			if(prods[k].base === hunter.base) {
+				prey = prods[k];
+				prods.splice(k,1);
+				matchPrice = parseFloat(hunter.basePrice) + parseFloat(prey.basePrice);
+				hunter.matchPrice = matchPrice;
+				prey.matchPrice = matchPrice;
+				MATCHES.push(hunter);
+				MATCHES.push(prey);
+				hunter = {};
+				prey = {};
+			}
 		}
 	}
-	
+
+	console.log('Done, check ' +PATH+ ' to see the ouput.');
 	//build the CSV and spit it out
 	var outputsie = ""
 	for(var p in MATCHES) {
@@ -104,5 +99,7 @@ function processinitArray(prods) {
 	}
 	var FILENAME = 'matchprices.csv';
 	fs.writeFileSync(PATH + FILENAME,outputsie);
+//	var rawSets = JSON.stringify(prods);
+//	fs.writeFileSync(PATH + "rawSets.txt",rawSets);
 }
 
